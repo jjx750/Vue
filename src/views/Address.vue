10ae9975631218ffdbfacb88e40be9b4f26a0b9e
xY <52590205+jjx750@users.noreply.github.com>
2020-03-19 02:07:04 +0800

    更新

---
+++ src/views/Address.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="address">
+    <addressEdit></addressEdit>
+  </div>
+</template>
+
+<script>
+import addressEdit from "@/components/Goods-address.vue";
+export default {
+  name: "Address",
+  components: { addressEdit },
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style>
+</style>
